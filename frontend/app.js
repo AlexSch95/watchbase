cfe0843b0bff0a3c7eef85d77b8adeac1b828796
OlgaTkach1989 <olga.tkachuk378@gmail.com>
2025-07-31 20:00:07 +0200

    Added rating

--- frontend/app.js
+++ frontend/app.js
@@ -1,19 +1,3 @@
-document.addEventListener("DOMContentLoaded", () => {
-  fetch("http://localhost:3000/api/movies")
-    .then(response => response.json())
-    .then(data => {
-      renderMovies(data); // функция отрисовки карточек
-    })
-    .catch(error => {
-      console.error("Fehler beim Laden der Filme:", error);
-    });
-});
-
-
-
-
-
-
 
 //
 
