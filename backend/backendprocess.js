b9aa323ae9fa76c43378a770962c5a06540b24ab
DerGreed <joel.hilberg@gmail.com>
2025-08-06 10:57:07 +0200

    genre ordered by name

--- backend/backendprocess.js
+++ backend/backendprocess.js
@@ -51,7 +51,7 @@ app.get("/api/movies", async (req, res) => {
 app.get("/api/genres", async (req, res)=>{
   try {
     const connection = await connectToDatabase();
-    const [rows] = await connection.execute('SELECT * FROM genres;')
+    const [rows] = await connection.execute('SELECT * FROM genres ORDER BY genre_name;')
     await connection.end();
     res.status(200).json(rows);
   } catch (error) {
