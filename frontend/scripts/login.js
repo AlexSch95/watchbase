04a32beee38b5d1d4567c8dcefd40594ab52a99a
DerGreed <joel.hilberg@gmail.com>
2025-08-17 14:24:40 +0200

    restored frontend

---
+++ frontend/scripts/login.js
@@ -0,0 +1,25 @@
+async function login() {
+  try {
+    const username = document.getElementById('loginUsername').value.trim();
+    const password = document.getElementById('loginPassword').value;
+    const response = await fetch('http://localhost:3000/api/users/login', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        username: username,
+        password: password
+      })
+    });
+    if (!response.ok) {
+      throw new Error(`HTTP Error: ${response.status}`);
+    }
+    const {token} = await response.json();
+    console.log(token);
+    localStorage.setItem('jwttoken', token);
+    window.location.href = "watchlist.html";
+
+  } catch (error) {
+    console.log('AAAAAAAAAA');
+    console.error(error);
+  }
+}
